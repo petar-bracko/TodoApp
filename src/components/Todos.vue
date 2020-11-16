2e175ec29bce59322dd11d414001acd0fc6ce207
Petar Bracko <petar.bracko@gmail.com>
2020-11-16 14:49:40 +0100

    Added animation for adding new todo to todos list

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
-    <div class="hide-overflow" v-if="Todos.length > 0">
+    <div class="hide-overflow">
+      <div
+        v-show="Todos.length == 0"
+        class="empty-active-todos-list animate__animated animate__fadeInDown anim-duration3"
+      >
+        <p>
+          Thera are no active Todos. You can add new ones, or activate old ones.
+        </p>
+      </div>
       <transition-group
         leave-active-class="animate__animated animate__fadeOutRight check-todo-duration"
+        enter-active-class="animate__animated animate__fadeInLeft check-todo-duration"
       >
         <div v-for="todo in Todos" :key="todo.id">
           <TodoItem :todo="todo" />
         </div>
       </transition-group>
-    </div>
-    <div v-else>
-      <div class="empty-active-todos-list">
-        <p>
-          Thera are no active Todos. You can add new ones, or activate old ones.
-        </p>
-      </div>
     </div>
   </div>
 </template>
